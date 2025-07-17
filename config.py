cbaa165f7bc8c3a6923044d79f6d56a42efe5799
LuisWilke <wilke_lopes@hotmail.com>
2025-07-16 22:29:49 -0400

    :recycle: Ajustando algumas funções e removendo comentários desnecessários

--- config.py
+++ config.py
@@ -2,14 +2,12 @@ import os
 from datetime import timedelta
 
 class Config:
-    # Configuração do banco de dados PostgreSQL
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "postgresql://postgres:postgres@localhost:5432/sistema_cobrancas"
+        "postgresql://postgres:postgres@localhost:5432/reguacobranca"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
-    # Configuração JWT
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "sua_chave_secreta_muito_segura_aqui")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "CHAVE_JWT")
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
